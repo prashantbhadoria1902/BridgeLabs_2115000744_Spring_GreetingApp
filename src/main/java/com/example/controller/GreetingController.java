8995697984b8614806ef0057c912ad5d13a0d81a
prashantbhadoria1902 <prashantbhadoria1902@gmail.com>
2025-03-03 16:03:24 +0530

    [Prashant] Added UC1 assignment

---
+++ src/main/java/com/example/controller/GreetingController.java
@@ -0,0 +1,39 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    @GetMapping
+    public Map<String, String> getGreeting() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello, GET Request!");
+        return response;
+    }
+
+    @PostMapping
+    public Map<String, String> postGreeting() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello, POST Request!");
+        return response;
+    }
+
+    @PutMapping
+    public Map<String, String> putGreeting() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello, PUT Request!");
+        return response;
+    }
+
+    @DeleteMapping
+    public Map<String, String> deleteGreeting() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello, DELETE Request!");
+        return response;
+    }
+}
